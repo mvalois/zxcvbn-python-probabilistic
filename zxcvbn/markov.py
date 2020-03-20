5d72bb7e221bbf712a0a246f6601f24b718d9d86
Mathieu Valois <mathieu.valois@unicaen.fr>
2020-03-20 16:09:55 +0100

    Working Markov model & MC sampling

--- zxcvbn/markov.py
+++ zxcvbn/markov.py
@@ -1,36 +1,41 @@
 import os
-from .probabilistic_model import ProbabilisticModel
+import pickle
+import random
+from probabilistic_model import ProbabilisticModel
 
 from ngram import NGram as ngram
 
 from collections import defaultdict as ddict
 from types import SimpleNamespace
 
+S = chr(0)
+E = chr(1)
+
 class Markov(ProbabilisticModel):
 	def __init__(self, filename, n=10**5, count=False, N=3, nblevel=10):
-		self.probas = ddict(ddict(float))
-		self.nb_ngrams = ddict(int)
+		self.probas = ddict()
 		self.N = N
 		self._ngrams_obj = ngram(N=self.N)
 		super().__init__(filename, n, count)
 
 	def dump(self, filename):
-		pickle.dump((self.probas, self.nb_grams, self.N), open(filename, 'wb'))
+		pickle.dump((self.probas, self.N, self.sample), open(filename, 'wb'))
 
 	def load(self, filename):
-		self.probas, self.nb_grams, self.N = pickle.load(open(filename, 'rb'))
+		self.probas, self.N, self.sample = pickle.load(open(filename, 'rb'))
 
 	def learn(self, filename):
 		with open(filename) as inbuff:
 			for word in inbuff:
-				word = word.rstrip()
+				word = word.strip()
 				if self.count:
-					word, count = word.split(maxsplit=1)
+					count, word = word.split(maxsplit=1)
 					count = int(count)
 				else:
 					count = 1
-				self.parse(word, count)
-		for lhs, rhs in self.probas:
+				if len(word) >= self.N:
+					self.parse(word, count)
+		for lhs, rhs in self.probas.items():
 			k = sum(rhs.values())
 			for g in rhs:
 				rhs[g] /= k
@@ -38,20 +43,40 @@ class Markov(ProbabilisticModel):
 	def proba(self, word):
 		p = 1
 		gramms = [g for g in self._ngrams_obj.ngrams(word)]
-		if gramms[0] not in self.probas[chr(0)]:
+		if gramms[0][:-1] not in self.probas[S]:
 			return 0
-		p *= self.probas[chr(0)][gramms[0]]
-		if gramms[-1] not in self.probas[chr(1)]:
+		p *= self.probas[S][gramms[0][:-1]]
+		if E not in self.probas[gramms[-1][1:]]:
 			return 0
-		p *= self.probas[chr(1)][gramms[-1]]
-		for i, g in zip(range(1+len(gramms)), gramms[1:-1]):
-			if g not in self.probas[gramms[i-1]]:
+		p *= self.probas[gramms[-1][1:]][E]
+		for g in gramms:
+			if g not in self.probas[g[1:]]:
 				return 0
-			p *= self.probas[gramms[i-1][1:]]
+			p *= self.probas[g[1:]][g]
 		return p
 
+	def odict(self, d):
+		pop = ([],[])
+		for k, v in d.items():
+			pop[0].append(k)
+			pop[1].append(v)
+		return pop
+
 	def monte_carlo_sample(self, n):
-		pass
+		psamples = list()
+		for i in range(n):
+			pop = self.odict(self.probas[S])
+			g = random.choices(pop[0], pop[1], k=1)[0]
+			p = self.probas[S][g]
+			while True:
+				pop = self.odict(self.probas[g])
+				ng = random.choices(pop[0], pop[1], k=1)[0]
+				p *= self.probas[g][ng]
+				if ng == E:
+					break
+				g = ng[1:]
+			psamples.append(p)
+		return psamples
 
 	def parse(self, word, count):
 		"""
@@ -59,7 +84,16 @@ class Markov(ProbabilisticModel):
 		and store number of occurrences
 		"""
 		gramms = [g for g in self._ngrams_obj.ngrams(word)]
-		self.probas[chr(0)][gramms[0]] += count
-		self.probas[gramms[-1]][chr(1)] += count
-		for i, g in zip(range(1+len(gramms)), gramms[1:-1]):
-			self.probas[gramms[i-1][1:]] += count
+		if S not in self.probas:
+			self.probas[S] = ddict(float)
+		self.probas[S][gramms[0][:-1]] += count
+
+		if gramms[-1][1:] not in self.probas:
+			self.probas[gramms[-1][1:]] = ddict(float)
+		self.probas[gramms[-1][1:]][E] += count
+
+		for g in gramms:
+			before = g[:-1]
+			if before not in self.probas:
+				self.probas[before] = ddict(float)
+			self.probas[before][g] += count
